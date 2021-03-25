714469d1d70a08d9eeb251c153ade7f2e61f98ed
naimo84 <git@naimo84.dev>
2021-03-25 09:09:35 +0100

    feat: rmdir before sync

--- src/gitsync.ts
+++ src/gitsync.ts
@@ -30,6 +30,7 @@ module.exports = function (RED: any) {
     async function processInput(node, msg: NodeMessageInFlow, send: (msg: NodeMessage | NodeMessage[]) => void, done: (err?: Error) => void, config) {
         try {
             const git: SimpleGit = simpleGit();
+            fs.rmdirSync('source', { recursive: true });
             await git.clone(node.source, 'source')
             await git.cwd('source')
             await git.addRemote('secondary', node.destination)
